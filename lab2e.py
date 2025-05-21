fc556d49fd4b8eceeefe531b9793687c4e4d38b1
Thit Lwin Oo <132836663+who-is-M4TT@users.noreply.github.com>
2025-05-20 22:35:53 -0400

    tloo1-lab2-submission

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Author: Thit Lwin Oo
+# Author ID: 170549232
+# Date Created: 2025/05/20
+
+timer = int(10)
+while timer != 0:
+    print(timer)
+    timer -= 1
+print("blast off!")
+
---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Thit Lwin Oo
+# Author ID: 170549232
+# Date Created: 2025/05/20
+
+import sys
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer -= 1
+print("blast off!")
+
